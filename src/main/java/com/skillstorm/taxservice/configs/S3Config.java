9402bc02ea12d3ece28d579c56c7dd4be5b137ea
qhardwick <qhardwick@gmail.com>
2024-05-30 12:56:06 -0400

    added temp credential variables since no one will help me with the bucket

--- src/main/java/com/skillstorm/taxservice/configs/S3Config.java
+++ src/main/java/com/skillstorm/taxservice/configs/S3Config.java
@@ -2,6 +2,8 @@ package com.skillstorm.taxservice.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -10,8 +12,16 @@ public class S3Config {
 
     @Bean
     public S3Client s3() {
+
+        // Create a session token for temporary credentials. Ideally you would set up an endpoint to your bucket in your
+        // VPC so you don't have to use temporary credentials, but I can't figure that out and no one will help me so this is what I'm doing:
+        AwsSessionCredentials awsCreds = AwsSessionCredentials.create("${ACCESS_KEY}", "${SECRET_ACCESS_KEY}", "${SESSION_TOKEN}");
+
         // Bucket names are globally unique. The bucket you try to access must be within
         // the region specified here:
-        return S3Client.builder().region(Region.US_EAST_1).build();
+        return S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                .build();
     }
 }
