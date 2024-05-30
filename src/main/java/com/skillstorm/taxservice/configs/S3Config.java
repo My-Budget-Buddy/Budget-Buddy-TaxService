04462933e4ab4e61c7eee64b2755f166e00b8354
qhardwick <qhardwick@gmail.com>
2024-05-30 14:33:54 -0400

    disabled access keys test

--- src/main/java/com/skillstorm/taxservice/configs/S3Config.java
+++ src/main/java/com/skillstorm/taxservice/configs/S3Config.java
@@ -15,13 +15,13 @@ public class S3Config {
 
         // Create a session token for temporary credentials. Ideally you would set up an endpoint to your bucket in your
         // VPC so you don't have to use temporary credentials, but this will work when testing locally:
-        AwsSessionCredentials awsCreds = AwsSessionCredentials.create("${ACCESS_KEY}", "${SECRET_ACCESS_KEY}", "${SESSION_TOKEN}");
+        //AwsSessionCredentials awsCreds = AwsSessionCredentials.create("${ACCESS_KEY}", "${SECRET_ACCESS_KEY}", "${SESSION_TOKEN}");
 
         // Bucket names are globally unique. The bucket you try to access must be within
         // the region specified here:
         return S3Client.builder()
                 .region(Region.US_EAST_1)
-                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+                //.credentialsProvider(StaticCredentialsProvider.create(awsCreds))
                 .build();
     }
 }
